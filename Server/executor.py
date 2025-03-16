870a81ca1237e76bd09fc655f43becbbdfd2e437
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2025-03-16 13:03:09 +0200

    created Server code executor 	new file:   Server/executor.py

---
+++ Server/executor.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+import uuid
+from config import Config
+
+
+class CodeExecutor:
+
+	@staticmethod
+	def execute_code(file):
+		"""Processing code execution from the transferred file."""
+		if not file or file.filename == "":
+			return {
+			    "error": "File is not provided or does not have a name"
+			}, 400
+
+		# Generate a unique name for the file
+		filename = f"{uuid.uuid4().hex}.py"
+		filepath = os.path.join(Config.UPLOAD_FOLDER, filename)
+		file.save(filepath)
+
+		try:
+			# Run the code in a separate process
+			result = subprocess.run(["python", filepath],
+			                        capture_output=True,
+			                        text=True,
+			                        timeout=10)
+
+			output = result.stdout
+			error = result.stderr
+		except subprocess.TimeoutExpired:
+			return {"error": "Execution time out"}, 500
+		finally:
+			# Delete the file after execution
+			os.remove(filepath)
+
+		return {"output": output, "error": error}
