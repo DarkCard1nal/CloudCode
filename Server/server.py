df98931bf8be74d78f17c356bc92f3e959321a24
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2025-03-16 12:58:48 +0200

    created main server class 	new file:   Server/server.py

---
+++ Server/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from config import Config
+from executor import CodeExecutor
+import threading
+
+
+class CodeExecutionServer:
+
+	def __init__(self):
+		self.app = Flask(__name__)
+		self.setup_routes()
+
+	def setup_routes(self):
+		"""Set up API routes."""
+
+		@self.app.route("/execute", methods=["POST"])
+		def execute():
+			"""Processes requests for code execution."""
+			result = CodeExecutor.execute_code(request.files.get("file"))
+			return jsonify(result)
+
+	def run(self):
+		"""Launches the server in multi-threaded mode."""
+		self.app.run(debug=Config.DEBUG, port=Config.PORT, threaded=True)
