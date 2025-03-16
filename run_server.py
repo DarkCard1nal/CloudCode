1733e48344b1fa67bc201360db515718a23bf899
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2025-03-16 13:07:57 +0200

    created run_server 	new file:   Server/__init__.py 	new file:   run_server.py

---
+++ run_server.py
@@ -0,0 +1,7 @@
+from Server.server import CodeExecutionServer
+from Server.config import Config
+
+if __name__ == "__main__":
+	Config.init()  # Initializing the configuration
+	server = CodeExecutionServer()
+	server.run()
