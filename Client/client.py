f20194114f433be8ce1da3f908f80c4801c73465
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2025-03-16 14:02:51 +0200

    added calculation of the time spent 	modified:   Client/client.py

--- Client/client.py
+++ Client/client.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import threading
+import time
 from Client.config import Config
 
 
@@ -16,18 +17,37 @@ class CloudComputeClient:
 		with open(file_path, "rb") as f:
 			files = {"file": f}
 			headers = {"Authorization": f"Bearer {self.api_key}"}
-			response = requests.post(self.api_url, files=files, headers=headers)
 
-		return response.json()
+			start_time = time.time()
+			response = requests.post(self.api_url, files=files, headers=headers)
+			end_time = time.time()
+
+			execution_time = (end_time - start_time) * 1000
+			result = response.json()
+			result["execution_time_ms"] = execution_time
+
+		return result
 
 	def send_sequential(self):
 		"""Sends all files from TASKS_FOLDER one by one."""
 		print("\n--- Sending files one by one ---\n")
+
+		start_time = time.time()
+
 		for file_name in os.listdir(self.tasks_folder):
 			file_path = os.path.join(self.tasks_folder, file_name)
 			if file_name.endswith(".py"):
 				result = self.send_code(file_path)
-				print(f"File: {file_name}\nResult: {result}\n")
+				print(
+				    f"File: {file_name}\nResult: {result}\nExecution time: {result['execution_time_ms']:.2f} ms\n"
+				)
+
+		end_time = time.time()
+		total_execution_time = (end_time - start_time) * 1000
+
+		print(
+		    f"\nTotal execution time of sequential requests: {total_execution_time:.2f} ms\n"
+		)
 
 	def send_parallel(self):
 		"""Sends all files at once and waits for a response."""
@@ -40,6 +60,8 @@ class CloudComputeClient:
 			if file_name.endswith(".py"):
 				results[file_name] = self.send_code(file_path)
 
+		start_time = time.time()
+
 		# Run each request in a separate thread
 		for file_name in os.listdir(self.tasks_folder):
 			thread = threading.Thread(target=task, args=(file_name,))
@@ -50,6 +72,15 @@ class CloudComputeClient:
 		for thread in threads:
 			thread.join()
 
+		end_time = time.time()
+		total_execution_time = (end_time - start_time) * 1000
+
 		# Displaying the results
 		for file_name, result in results.items():
-			print(f"File: {file_name}\nResult: {result}\n")
+			print(
+			    f"File: {file_name}\nResult: {result}\nExecution time: {result['execution_time_ms']:.2f} ms\n"
+			)
+
+		print(
+		    f"\nTotal execution time of parallel requests: {total_execution_time:.2f} ms\n"
+		)
